8f310a03337b007452432fa8e80317a4ec982dfc
galthran-wq <levmorozov900@gmail.com>
2025-08-23 18:27:55 +0000

    user model

---
+++ server/alembic/versions/64cd4beb4a5a_users.py
@@ -0,0 +1,33 @@
+"""users
+
+Revision ID: 64cd4beb4a5a
+Revises: 
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+revision: str = '64cd4beb4a5a'
+down_revision: Union[str, None] = None
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    op.create_table('users',
+    sa.Column('id', sa.UUID(), nullable=False),
+    sa.Column('email', sa.String(), nullable=True),
+    sa.Column('password_hash', sa.String(), nullable=True),
+    sa.Column('is_verified', sa.Boolean(), nullable=True),
+    sa.Column('is_superuser', sa.Boolean(), nullable=True),
+    sa.Column('created_at', sa.DateTime(), nullable=True),
+    sa.PrimaryKeyConstraint('id'),
+    sa.UniqueConstraint('email')
+    )
+
+
+def downgrade() -> None:
+    op.drop_table('users')
